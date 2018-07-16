1dbd07d9aa9da9f3474edda4dfc8d70191785fe8
Sergiu <Sergiu@irinas-macbook-pro.home>
2018-07-16 23:34:31 +0200

    Description: adding production config special case (for PythonAnywhere deployment)

--- app/__init__.py
+++ app/__init__.py
@@ -20,9 +20,16 @@ login_manager = LoginManager()
 
 # create app object
 def create_app(config_name):
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+    if os.getenv('FLASK_CONFIG') == "production":
+        app = Flask(__name__)
+        app.config.update(
+            SECRET_KEY=os.getenv('SECRET_KEY'),
+            SQLALCHEMY_DATABASE_URI=os.getenv('SQLALCHEMY_DATABASE_URI')
+        )
+    else:
+        app = Flask(__name__, instance_relative_config=True)
+        app.config.from_object(app_config[config_name])
+        app.config.from_pyfile('config.py')
     db.init_app(app)
 
     login_manager.init_app(app)
